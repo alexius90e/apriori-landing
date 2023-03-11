673a32c078953153ef864168543c56030ffa337e
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-12 01:06:56 +0300

    fix: iremoved unused file

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.header-nav');
-
-const menuToggler = document.querySelector('.header-nav');
-
-menuToggler.addEventListener('click', () => {
-  menu.classList.toggle('visible');
-});
