9a6397be6024ba100772ff18576cd42b5c8b3b16
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-11 16:14:41 +0300

    feat: implemented categories feature

--- js/index.js
+++ js/index.js
@@ -7,3 +7,38 @@ new Swiper('#bannerSwiper', {
     prevEl: '.banner-button-prev',
   },
 });
+
+const categorySwiperthumbsOptions = {
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  centerInsufficientSlides: true,
+  breakpoints: {
+    480: {
+      slidesPerView: 5,
+    },
+    768: {
+      slidesPerView: 6,
+    },
+  },
+};
+
+const categorySwiperOptions = {
+  loop: true,
+  spaceBetween: 10,
+};
+
+const swiperCategoryThumbs1 = new Swiper('#category1 .category-thumbs', categorySwiperthumbsOptions);
+
+new Swiper('#category1 .category-items', {
+  ...categorySwiperOptions,
+  navigation: {
+    nextEl: '#category1 .swiper-button-next',
+    prevEl: '#category1 .swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiperCategoryThumbs1,
+  },
+});
